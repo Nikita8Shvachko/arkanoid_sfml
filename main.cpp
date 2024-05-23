3d8cdd5fa4a8bb9304d85c4faa58f85f851544c7
Nikita Shvachko <228nikitalik@gmail.com>
2024-05-23 20:12:54 +0300

    added all textures and created structs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+
+struct brick {
+    int hardness;
+    sf::Color color;
+    int x;
+    int y;
+};
+struct bonus : public brick {
+    std::string type;
+
+};
+struct paddle {
+    int x;
+    int y;
+    int width;
+    int height;
+};
+struct ball {
+    int x;
+    int y;
+    int dx;
+    int dy;
+    int power;
+};
+
 int main() {
     return 0;
 }
